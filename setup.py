e80cd270e796878bda6cd5042ee54c3e4caf742e
mblack <martin@parkopedia.com>
2018-07-09 20:56:24 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mblack20/rastertodataframe',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
