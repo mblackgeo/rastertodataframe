2182bf4af2c0ac07b93421af5f9948f5c1c088f7
mblack <martin@parkopedia.com>
2018-07-09 21:07:23 +0100

    Remove requirements to prevent build failures.

--- setup.py
+++ setup.py
@@ -11,13 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    "GDAL",
-    "geopandas",
-    "pandas",
-    "pyproj",
-    "numpy"
-]
+requirements = []
 
 setup_requirements = ['pytest-runner', ]
 
