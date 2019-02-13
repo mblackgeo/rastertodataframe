8426f2e237b1fdbc90c9baa23815bde46e9764ed
mblack <martin@parkopedia.com>
2019-02-13 10:16:30 +0000

    Add test coverage for single band rasters

--- tests/test_rastertodataframe.py
+++ tests/test_rastertodataframe.py
@@ -16,6 +16,7 @@ class TestRasterToDataFrame(unittest.TestCase):
         self.vector_path = os.path.join(test_data_path, 'vector.geojson')
         self.raster_wgs84_path = os.path.join(test_data_path,
                                               'raster_epsg4326.tif')
+        self.single_band_raster = os.path.join(test_data_path, 'oneband.tif')
 
     def test_raster_to_dataframe_with_vector(self):
         out_df = raster_to_dataframe(
@@ -33,4 +34,21 @@ class TestRasterToDataFrame(unittest.TestCase):
         expected_cols = ['Band_1', 'Band_2', 'Band_3', 'Band_4']
 
         self.assertEqual(out_df.shape, (2204, 4))
+        self.assertCountEqual(list(out_df.columns), expected_cols)
+
+    def test_single_band_with_vector(self):
+        out_df = raster_to_dataframe(
+            self.single_band_raster, vector_path=self.vector_path)
+
+        expected_cols = ['Band_1', 'fid', 'value', 'value_string']
+
+        self.assertEqual(out_df.shape, (267, 4))
+        self.assertCountEqual(list(out_df.columns), expected_cols)
+
+    def test_single_band_without_vector(self):
+        out_df = raster_to_dataframe(self.single_band_raster)
+
+        expected_cols = ['Band_1']
+
+        self.assertEqual(out_df.shape, (2262, 1))
         self.assertCountEqual(list(out_df.columns), expected_cols)
